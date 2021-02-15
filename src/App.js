28a0a3bfc1414e4ecce7e33f76c028f306fdba52
Vladimir <vladgoloyan@gmail.com>
2021-02-15 20:05:24 +0400

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Header from 'components/Header/Header';
+import Layout from 'components/Layout/Layout';
+import Container from 'containers/Container/Container';
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Layout>
+          <Container />
+        </Layout>
+      </div>
+    )
+  }
+}
+
+export default App;
